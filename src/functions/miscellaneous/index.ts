390a684dbb4b7dfb018dcb97019da51856d0e275
AlexMan123456 <alex.man03888@gmail.com>
2026-02-05 01:26:53 +0000

    Add one of the coolest tests to exist in this package
    Is it overkill? Probably, yeah. But is it cool. Also heck yeah!

--- src/functions/miscellaneous/index.ts
+++ src/functions/miscellaneous/index.ts
@@ -2,6 +2,7 @@ export { default as convertFileToBase64 } from "src/functions/miscellaneous/conv
 export { default as createFormData } from "src/functions/miscellaneous/createFormData";
 export { default as getRandomNumber } from "src/functions/miscellaneous/getRandomNumber";
 export { default as isOrdered } from "src/functions/miscellaneous/isOrdered";
+export { default as sayHello } from "src/functions/miscellaneous/sayHello";
 export { default as stringifyDotenv } from "src/functions/miscellaneous/stringifyDotenv";
 export { default as stringListToArray } from "src/functions/miscellaneous/stringListToArray";
 export { default as wait } from "src/functions/miscellaneous/wait";
