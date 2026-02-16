cd61a46b19cdff2cf6f7840740c0a6905b88ea51
AlexMan123456 <alex.man03888@gmail.com>
2026-02-16 01:26:10 +0000

    Get rid of getInterpolations and getPublicAndPrivateKey
    getInterpolations has been replaced with getStringsAndInterpolations, and getPublicAndPrivateKey can now be replaced with sodium.crypto_box_keypair (as the issue it was trying to solve got patched recently).

--- src/root/functions/security/index.ts
+++ src/root/functions/security/index.ts
@@ -1,4 +1,1 @@
 export { default as encryptWithKey } from "src/root/functions/security/encryptWithKey";
-export { default as getPublicAndPrivateKey } from "src/root/functions/security/getPublicAndPrivateKey";
-
-export type { PublicAndPrivateKey } from "src/root/functions/security/getPublicAndPrivateKey";
