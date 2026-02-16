a30549bec25935ce15c1ff41a4d85a518f08a7f8
AlexMan123456 <alex.man03888@gmail.com>
2026-02-16 01:00:33 +0000

    Remove deprecated versioning functions
    They have been replaced with the VersionNumber class.

--- src/root/functions/versioning/index.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-deprecated */
-export { default as determineVersionType } from "src/root/functions/versioning/determineVersionType";
-export { default as getIndividualVersionNumbers } from "src/root/functions/versioning/getIndividualVersionNumbers";
-export { default as incrementVersion } from "src/root/functions/versioning/incrementVersion";
-export { default as parseVersion } from "src/root/functions/versioning/parseVersion";
-
-export type { IncrementVersionOptions } from "src/root/functions/versioning/incrementVersion";
-export type { ParseVersionOptions } from "src/root/functions/versioning/parseVersion";
