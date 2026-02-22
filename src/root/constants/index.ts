ce8c58c775c1fe1ecf7cff8f37de70dc63e1a22c
AlexMan123456 <alex.man03888@gmail.com>
2026-02-22 20:38:07 +0000

    Add a 24-hours in milliseconds constant
    Will be used in alex-c-line's update notifications because JavaScript Date sucks.
    
    This absolutely is getting deprecated the moment Temporal comes out.

--- src/root/constants/index.ts
+++ src/root/constants/index.ts
@@ -1,3 +1,4 @@
 export { default as FILE_PATH_REGEX } from "src/root/constants/FILE_PATH_REGEX";
 export { default as NAMESPACE_EXPORT_REGEX } from "src/root/constants/NAMESPACE_EXPORT_REGEX";
+export { default as ONE_DAY_IN_MILLISECONDS } from "src/root/constants/ONE_DAY_IN_MILLISECONDS";
 export { default as VERSION_NUMBER_REGEX } from "src/root/constants/VERSION_NUMBER_REGEX";
