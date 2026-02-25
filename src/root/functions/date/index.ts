066e43f7e03062ca8dfe3c0c2330355fbef3a027
AlexMan123123 <alex.man@curaleafint.com>
2026-02-25 19:48:29 +0000

    Add calculateMonthlyDifference
    Useful for calculating monthly differences

--- src/root/functions/date/index.ts
+++ src/root/functions/date/index.ts
@@ -1,4 +1,5 @@
 export { default as addDaysToDate } from "src/root/functions/date/addDaysToDate";
+export { default as calculateMonthlyDifference } from "src/root/functions/date/calculateMonthlyDifference";
 export { default as formatDateAndTime } from "src/root/functions/date/formatDateAndTime";
 export { default as isAnniversary } from "src/root/functions/date/isAnniversary";
 export { default as isLeapYear } from "src/root/functions/date/isLeapYear";
