86c432493831f1a9619bc58526e17a544c666719
AlexMan123456 <alex.man03888@gmail.com>
2026-02-25 01:28:06 +0000

    Export parseUUID

--- src/root/functions/parsers/index.ts
+++ src/root/functions/parsers/index.ts
@@ -2,6 +2,7 @@ export { default as parseBoolean } from "src/root/functions/parsers/parseBoolean
 export { default as parseEnv, Env } from "src/root/functions/parsers/parseEnv";
 export { default as parseFormData } from "src/root/functions/parsers/parseFormData";
 export { default as parseIntStrict } from "src/root/functions/parsers/parseIntStrict";
+export { default as parseUUID } from "src/root/functions/parsers/parseUUID";
 export {
   default as parseVersionType,
   VersionType,
