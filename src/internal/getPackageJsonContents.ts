dcfcdb8872c663ce9f8758e61e38a3032b967a88
AlexMan123456 <alex.man03888@gmail.com>
2026-03-01 16:49:54 +0000

    Add a strict mode option to getPackageJsonContents
    This allows us to reduce the need to have that null check everywhere, instead replacing it with a strict mode option that, if enabled, will throw the packageJsonNotFoundError. It must be explicitly opted out of using strict: false.

--- src/internal/getPackageJsonContents.ts
+++ src/internal/getPackageJsonContents.ts
@@ -1,13 +1,28 @@
+import type { NullableOnCondition } from "src/root";
+
 import { readFile } from "node:fs/promises";
 
 import getPackageJsonPath from "src/internal/getPackageJsonPath";
+import packageJsonNotFoundError from "src/internal/packageJsonNotFoundError";
 
-async function getPackageJsonContents(directory: string): Promise<Record<string, any> | null> {
+export interface GetPackageJsonContentsOptions<Strict extends boolean = true> {
+  strict?: Strict;
+}
+
+async function getPackageJsonContents<Strict extends boolean = true>(
+  directory: string,
+  options?: GetPackageJsonContentsOptions<Strict>,
+): Promise<NullableOnCondition<Strict, Record<string, any>>> {
+  const { strict = true } = options ?? {};
   try {
     return JSON.parse(await readFile(getPackageJsonPath(directory), "utf-8"));
   } catch (error) {
     if (error instanceof Error && "code" in error && error.code === "ENOENT") {
-      return null;
+      if (strict) {
+        throw packageJsonNotFoundError(directory);
+      } else {
+        return null as NullableOnCondition<Strict, Record<string, any>>;
+      }
     }
     throw error;
   }
