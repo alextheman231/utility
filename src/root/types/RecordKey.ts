4066a396d6c7781b51f9b940b30868a60f332e3e
AlexMan123456 <alex.man03888@gmail.com>
2026-03-21 18:06:35 +0000

    Deprecate RecordKey
    There actually already exists a native PropertyKey type that is exactly what RecordKey is, so we can just use that instead.

--- src/root/types/RecordKey.ts
+++ src/root/types/RecordKey.ts
@@ -2,5 +2,7 @@
  * Represents the native Record's possible key type.
  *
  * @category Types
+ *
+ * @deprecated Please use the native `PropertyKey` type instead.
  */
 export type RecordKey = string | number | symbol;
