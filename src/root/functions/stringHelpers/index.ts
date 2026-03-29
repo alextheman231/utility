986919223b67b9d528602a634152831798e08b23
AlexMan123456 <alex.man03888@gmail.com>
2026-03-29 16:36:17 +0100

    Add an escapeRegexPattern function
    This helps to ensure that any raw Regex strings we pass into RegExp gets properly escaped before we use it. This will be especially helpful when we introduce Wouter to components, as we're likely to be using Regex a lot to get the necessary parameters.

--- src/root/functions/stringHelpers/index.ts
+++ src/root/functions/stringHelpers/index.ts
@@ -1,5 +1,6 @@
 export { default as appendSemicolon } from "src/root/functions/stringHelpers/appendSemicolon";
 export { default as camelToKebab } from "src/root/functions/stringHelpers/camelToKebab";
+export { default as escapeRegexPattern } from "src/root/functions/stringHelpers/escapeRegexPattern";
 export { default as kebabToCamel } from "src/root/functions/stringHelpers/kebabToCamel";
 export { default as truncate } from "src/root/functions/stringHelpers/truncate";
 
