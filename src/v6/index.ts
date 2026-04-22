829487ea88b2ef2d0df108dbe82d87bd08588cc3
AlexMan123456 <alex.man03888@gmail.com>
2026-04-22 23:08:01 +0100

    Copy the logic for sayHello into each entrypoint
    This prevents potential cross-contamination of entrypoints, which defeats the whole purpose of having entrypoints.

--- src/v6/index.ts
+++ src/v6/index.ts
@@ -1,6 +1,6 @@
-export { default as sayHello } from "src/root/functions/miscellaneous/sayHello";
 export { default as CodeError } from "src/v6/CodeError";
 export { default as DataError, DataErrorCode } from "src/v6/DataError";
+export { default as sayHello } from "src/v6/sayHello";
 
 export type { IsTypeArgumentString } from "src/root/types/IsTypeArgumentString";
 export type { ExpectErrorOptions } from "src/v6/DataError";
