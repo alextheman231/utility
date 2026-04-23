e1a2517a1db7f21914052b527404ae54af2a4e14
AlexMan123456 <alex.man03888@gmail.com>
2026-04-23 01:27:57 +0100

    Export values from the deprecated folder
    Previously it was just exporting types, meaning that the DataError value was not being exported too...

--- src/root/index.ts
+++ src/root/index.ts
@@ -1,5 +1,4 @@
 export * from "src/root/constants";
+export * from "src/root/deprecated";
 export * from "src/root/functions";
 export * from "src/root/types";
-
-export type * from "src/root/deprecated";
