7221a18a4217b0af7d6a92cfced76cf0babbfa9b
AlexMan123456 <alex.man03888@gmail.com>
2026-04-24 01:30:06 +0100

    Make the Zod parsing read a little more naturally
    Instead of doing the slightly awkward thing where we have to pass the schema and the input into parseZodSchema, we can make it read more like natural Zod by adding a .with() method where we pass the schema first, followed by chaining it with .parse() that takes the data. It feels less confusing as to which order they should be passed in and reads more like natural Zod.

--- src/root/zod/az.ts
+++ src/root/zod/az.ts
@@ -1,6 +1,7 @@
 import type { ZodCoercedDate, ZodCoercedNumber, ZodType } from "zod";
 
 import type { VersionNumber } from "src/root/types";
+import type { DataError } from "src/v6";
 
 import z from "zod";
 
@@ -21,6 +22,26 @@ const az = {
   },
   fieldDate: (): ZodCoercedDate<string | null> => {
     return z.coerce.date<string | null>();
+  },
+  /* eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types -- 
+    The typing here is already very clear and explicit as is.
+    I don't feel there's any benefit to typing the return of the with method further as I feel it would become harder to read. 
+  */
+  with: <SchemaType extends ZodType>(schema: SchemaType) => {
+    return {
+      parse: <ErrorType extends Error = DataError>(
+        input: unknown,
+        error?: ErrorType | ((zodError: z.ZodError<unknown>) => void | ErrorType),
+      ): ReturnType<typeof parseZodSchema<SchemaType, ErrorType>> => {
+        return parseZodSchema<SchemaType, ErrorType>(schema, input, error);
+      },
+      parseAsync: async <ErrorType extends Error = DataError>(
+        input: unknown,
+        error?: ErrorType | ((zodError: z.ZodError<unknown>) => void | ErrorType),
+      ): ReturnType<typeof parseZodSchemaAsync<SchemaType, ErrorType>> => {
+        return await parseZodSchemaAsync<SchemaType, ErrorType>(schema, input, error);
+      },
+    };
   },
 };
 
