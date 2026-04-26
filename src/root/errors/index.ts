242d6f191ce9a5495d5b9ab749fda7030999cf68
AlexMan123456 <alex.man03888@gmail.com>
2026-04-26 02:39:05 +0100

    Export assertNotNull

---
+++ src/root/errors/index.ts
@@ -0,0 +1,1 @@
+export { default as assertNotNull } from "src/root/errors/assertNotNull";
--- src/root/index.ts
+++ src/root/index.ts
@@ -1,5 +1,6 @@
 export * from "src/root/constants";
 export * from "src/root/deprecated";
+export * from "src/root/errors";
 export * from "src/root/functions";
 export * from "src/root/types";
 export * from "src/root/zod";
