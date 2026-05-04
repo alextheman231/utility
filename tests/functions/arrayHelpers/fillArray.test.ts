92edbe3b0626cf3004ed7d4fba7d001946dd9e0a
AlexMan123456 <alex.man03888@gmail.com>
2026-05-04 13:25:31 +0100

    Allow fillArray to take a sequential option
    Needed for Lexicon tests so we don't call the Postgres client multiple times in parallel.

--- tests/functions/arrayHelpers/fillArray.test.ts
+++ tests/functions/arrayHelpers/fillArray.test.ts
@@ -26,12 +26,21 @@ describe("fillArray", () => {
     ).toEqual([0, 1, 2, 3, 4]);
   });
   test("Works with async functions", async () => {
-    expect(
-      await fillArray(async (index) => {
-        await wait(0.1);
-        return index;
-      }, 5),
-    ).toEqual([0, 1, 2, 3, 4]);
+    const result = fillArray(async (index) => {
+      await wait(0.1);
+      return index;
+    }, 5);
+
+    expect(result).toBeInstanceOf(Promise);
+    await expect(result).resolves.toEqual([0, 1, 2, 3, 4]);
+  });
+  test("Treats mixed sync/async results as async", async () => {
+    const result = fillArray((index) => {
+      return index === 1 ? Promise.resolve(index) : index;
+    }, 3);
+
+    expect(result).toBeInstanceOf(Promise);
+    await expect(result).resolves.toEqual([0, 1, 2]);
   });
   test("Infers correct types", async () => {
     expectTypeOf(
@@ -46,5 +55,54 @@ describe("fillArray", () => {
         return "Hello";
       }),
     ).toEqualTypeOf<Promise<Array<string>>>();
+  });
+  test("Runs in parallel by default", async () => {
+    const order: Array<number> = [];
+
+    const result = await fillArray(async (index) => {
+      await wait((10 - index * 2) / 1000);
+      order.push(index);
+      return index;
+    }, 3);
+
+    expect(result).toEqual([0, 1, 2]);
+    expect(order).not.toEqual([0, 1, 2]);
+  });
+  test("Resolves sequentially when sequential option is true.", async () => {
+    const executionOrder: Array<number> = [];
+
+    const result = await fillArray(
+      async (i) => {
+        executionOrder.push(i);
+        await wait(0.005);
+        return i;
+      },
+      3,
+      { sequential: true },
+    );
+
+    expect(result).toEqual([0, 1, 2]);
+    expect(executionOrder).toEqual([0, 1, 2]);
+  });
+  test("Does not start next task until previous resolves in sequential mode.", async () => {
+    const timestamps: Array<number> = [];
+
+    await fillArray(
+      async () => {
+        timestamps.push(Date.now());
+        await wait(0.02);
+      },
+      3,
+      { sequential: true },
+    );
+
+    const diffs = timestamps.slice(1).map((timestamp, index) => {
+      return timestamp - timestamps[index];
+    });
+    expect(
+      diffs.every((diff) => {
+        return diff >= 15;
+      }),
+    ).toBe(true);
   });
 });
