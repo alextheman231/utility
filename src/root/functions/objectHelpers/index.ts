4be633239f67e3f004f0eff09c4b19d89a8193db
AlexMan123456 <alex.man03888@gmail.com>
2026-06-18 00:35:04 +0100

    Add a pickProperties function
    This is essentially the complement of omitProperties, where we instead reconstruct an object based on the keys we want, rather than what we don't want.

--- src/root/functions/objectHelpers/index.ts
+++ src/root/functions/objectHelpers/index.ts
@@ -1,5 +1,6 @@
 export { default as getRecordKeys } from "src/root/functions/objectHelpers/getRecordKeys";
 export { default as omitProperties } from "src/root/functions/objectHelpers/omitProperties";
+export { default as pickProperties } from "src/root/functions/objectHelpers/pickProperties";
 export { default as removeUndefinedFromObject } from "src/root/functions/objectHelpers/removeUndefinedFromObject";
 
 export type { RemoveUndefined } from "src/root/functions/objectHelpers/removeUndefinedFromObject";
